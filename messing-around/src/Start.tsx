b9c9b2c5c3ac39310552b184a12961b8d065c11f
defcarlito <harrisoncamorim@gmail.com>
2025-05-29 17:15:41 -0500

    added image 2

--- messing-around/src/Start.tsx
+++ messing-around/src/Start.tsx
@@ -8,7 +8,7 @@ function StartPage() {
         <div>
 
             <h1>pick either side!</h1>
-            <img className="img-haircut" src="./src/assets/dream_haircut.JPG"/><br/>
+            <img className="img-haircut" src="/dream_haircut.JPG"/><br/>
 
             <button className='harry-buttons' onClick={() => navigate('/harrison')}>Harrison</button>
             <button className='sohum-buttons' onClick={() => navigate('/sohum')}>Sohum</button>
