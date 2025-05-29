bda65f04a521e28eb1d352f1e4a2b3d002532333
defcarlito <harrisoncamorim@gmail.com>
2025-05-29 15:56:56 -0500

    test

--- messing-around/src/Start.tsx
+++ messing-around/src/Start.tsx
@@ -8,7 +8,7 @@ function StartPage() {
 
     return(
         <div>
-            <h1>choose a side</h1>
+            <h1>pick harrison button</h1>
             <button className='harry-buttons' onClick={() => navigate('/harrison')}>Harrison</button>
             <button className='sohum-buttons' onClick={() => navigate('/sohum')}>Sohum</button>
         </div>
