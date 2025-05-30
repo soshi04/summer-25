b6f6e9529cb0a6531ef1c5eea6b4c9e204f463f6
soshi04 <sirsohumjoshi@gmail.com>
2025-05-30 13:47:05 -0500

    added leetcode tracker

--- messing-around/src/pages/sohum/MainPageS.tsx
+++ messing-around/src/pages/sohum/MainPageS.tsx
@@ -1,13 +1,16 @@
-import { useNavigate } from 'react-router-dom' 
+import { useNavigate, Link } from 'react-router-dom' 
 
 function MainPageS() {
     const navigate = useNavigate()
 
     return (
         <>
-        <h1>Hello Sohum</h1>
-        <p>testing s</p>
-        <button onClick={() => navigate('/')}>back to main</button>
+        <h1>Sohum's Page</h1>
+        <p>Click on the link to go to my first project!</p>
+
+        <Link to="https://leetcode-suggest-and-track.vercel.app/">First Project</Link>
+        <button onClick={() => navigate('/')}>Back to Main Page</button>
+
         </>
     )
 }
