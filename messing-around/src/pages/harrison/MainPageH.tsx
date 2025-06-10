979ea2803d6613e6c9a5c18e1b274f05c4cbffbf
defcarlito <harrisoncamorim@gmail.com>
2025-06-09 20:56:46 -0500

    boom

--- messing-around/src/pages/harrison/MainPageH.tsx
+++ messing-around/src/pages/harrison/MainPageH.tsx
@@ -1,10 +1,23 @@
+import './harrison.css'
+
 import { useNavigate } from 'react-router-dom' 
 import { useEffect, useState} from 'react';
 
 function MainPageH() {
     const navigate = useNavigate()
     const [ip, setIp] = useState('');
+    const [flash, setFlash] = useState(false)
     
+    function boom(){
+        const audio = new Audio('audio/mysterious.mp3')
+        audio.play()
+    }
+
+    function triggerFlash(){
+        setFlash(true)
+        boom()
+        setTimeout(() => setFlash(false), 500)
+    }
 
     // changing some stuff test
     useEffect(() => {
@@ -28,6 +41,8 @@ function MainPageH() {
             <h3>{ip}</h3>
             <p>{location ? `Thanks! You're in ${location.city}, ${location.region}` : 'Loading your exact location...'}</p>
             <h2>Added to the database, GG</h2>
+            <img className={`dog ${flash ? 'flash' : ''}`} src="dog.jpg" />
+            <button onClick={triggerFlash}>very special button here</button>
             <button onClick={() => navigate('/')}>back to main</button>
         </div>
     )
